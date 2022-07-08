54841f67218270199bf4213214736c3eb56452a2
Carlos Zacarias <emii.zacarias@gmail.com>
2022-07-08 01:26:18 -0300

    first commit

---
+++ EjerciciosBootcamp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EjerciciosBootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre: ");
+            String nombre = Console.ReadLine();
+
+            String hora = DateTime.Now.ToString("hh:mm");
+
+            Console.WriteLine("Hola " + nombre +", la hora es: "+hora);
+
+
+        }
+    }
+}

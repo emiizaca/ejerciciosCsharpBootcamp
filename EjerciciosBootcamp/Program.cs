b6c4b0cc280c7d34e5ba002319adbebc2008617a
Carlos Zacarias <emii.zacarias@gmail.com>
2022-07-08 19:38:30 -0300

    more exercise

--- EjerciciosBootcamp/Program.cs
+++ EjerciciosBootcamp/Program.cs
@@ -6,14 +6,61 @@ namespace EjerciciosBootcamp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese su nombre: ");
+            //Console.WriteLine("Ingrese su nombre: ");
+            //String nombre = Console.ReadLine();
+
+            //String hora = DateTime.Now.ToString("hh:mm");
+
+            //Console.WriteLine("Hola " + nombre +", la hora es: "+hora);
+
+            Console.WriteLine("Nombre? ");
             String nombre = Console.ReadLine();
+            Console.WriteLine("Apellido? ");
+            String apellido = Console.ReadLine();
+            Console.WriteLine("Edad? ");
+            String edad = Console.ReadLine();
+            Console.WriteLine("Sabe Programar? ");
+            String sabeProgramar = Console.ReadLine();
 
-            String hora = DateTime.Now.ToString("hh:mm");
+            Console.WriteLine("El Sr. " + nombre + " " + apellido + " tiene " + edad + " años de edad y " + sabeProgramar + " sabe programar.");
 
-            Console.WriteLine("Hola " + nombre +", la hora es: "+hora);
+            int numero = 25;
+            if (numero > 18)
+            {
+                Console.WriteLine("El número es mayor de 18.");
+            }
 
+            char caracter = 'a';
+            if(caracter == 'a')
+            {
+                Console.WriteLine("El caracter es igual a a");
+            }
 
+            if (numero > 18 && caracter == 'a')
+            {
+                Console.WriteLine("Se cumplen las 2 condiciones.");
+            }
+
+            if (numero < 18 || caracter == 'a')
+            {
+                Console.WriteLine("Se cumple al menos una condicion.");
+            }
         }
+    }
+
+    class Coche
+    {
+        int puertas;
+        int ruedas;
+        String marca;
+        bool ITVvigente;
+    }
+
+    class Mesa
+    {
+        float peso;
+        float largo;
+        String material;
+        String color;
     }
 }
